4298464ab9cc7c372fe666910181f3654d576053
patilyogin96 <96yoginpatil@gmail.com>
2022-07-14 14:36:12 +0530

    Updates

---
+++ precache-manifest.e14fb2d7adc4ef728c63dc6700ede8cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72643926a960526ee509",
+    "url": "/react_cart/static/css/main.38699c8d.chunk.css"
+  },
+  {
+    "revision": "72643926a960526ee509",
+    "url": "/react_cart/static/js/main.72643926.chunk.js"
+  },
+  {
+    "revision": "2c80ac1fe2aa0d16567f",
+    "url": "/react_cart/static/js/1.2c80ac1f.chunk.js"
+  },
+  {
+    "revision": "5b58c7919f0bc6503bf6",
+    "url": "/react_cart/static/js/2.5b58c791.chunk.js"
+  },
+  {
+    "revision": "eb7b0b949e5025522a11",
+    "url": "/react_cart/static/js/runtime~main.eb7b0b94.js"
+  },
+  {
+    "revision": "f219133ed0669fd35f6abb4a9d21c777",
+    "url": "/react_cart/index.html"
+  }
+];
